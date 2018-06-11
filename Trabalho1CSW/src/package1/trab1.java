97d115b97f6ffc1950da0c0f7a933a0fcd6b497f
Uriel Battanoli <urielbattanoli@gmail.com>
2018-06-11 19:30:47 -0300

    Updating trab1

--- Trabalho1CSW/src/package1/trab1.java
+++ Trabalho1CSW/src/package1/trab1.java
@@ -12,40 +12,52 @@ public class trab1 {
 
 	
 	public static void main(String[] args) {
-//		MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://localhost:27017"));
-//		MongoDatabase database = mongoClient.getDatabase("Banco");
-//		MongoCollection<Document> collection = database.getCollection("Collection1");
-//		
-//		Document person = new Document("id", 1)
-//				.append("name", "uriel")
-//				.append("age", 22)
-//				.append("address", new Document("line1", "heuheu")
-//						.append("number", "229"));
-//		
-//		collection.insertOne(person);
-//		
-//		BasicDBObject query = new BasicDBObject("id", 1);
-//		collection.replaceOne(query, person);
-//		
-//
-//		MongoCursor<Document> cursor = collection.find().iterator();
-//		while(cursor.hasNext()) {
-//			Document obj = cursor.next();
-//			System.out.println(obj.get("name"));
-//		}
-//		
-//		mongoClient.close();
-		
 		GUDatabaseManager manager = new GUDatabaseManager();
-		Pessoa p = new Pessoa();
-//		p.cpf = "00109430212";
-//		p.idade = 24;
-		p.nome = "Ronaldo";
-		p.setId(1);
-		p.setTableName("Pessoa");
-//		manager.createObject(p);
-		manager.updateObject(p);
-		manager.readObject(p);
-		System.out.println(p.nome);
+
+		Pessoa pessoa = new Pessoa();
+		pessoa.setId(1);
+		pessoa.setTableName("Pessoa");
+
+		pessoa.cpf = "00109430212";
+		pessoa.idade = 24;
+		pessoa.nome = "Ronaldo";
+
+		// create
+		manager.createObject(pessoa);
+
+		System.out.println(pessoa.nome);
+		System.out.println(pessoa.cpf);
+		System.out.println(pessoa.idade);
+
+		System.out.println("-------------------");
+		
+		// to update edit the object and call updateObject()
+		pessoa.nome = "Roberto";
+		// update
+		manager.updateObject(pessoa);
+
+		System.out.println(pessoa.nome);
+		System.out.println(pessoa.cpf);
+		System.out.println(pessoa.idade);
+
+		System.out.println("-------------------");
+		
+		// to fetch from database you need to create a object from the
+		// respective class and then set the id you want to query
+		Pessoa mesmaPessoa = new Pessoa();
+		mesmaPessoa.setId(1);
+		mesmaPessoa.setTableName("Pessoa");
+
+		// read
+		mesmaPessoa = (Pessoa) manager.readObject(mesmaPessoa);
+
+		System.out.println(mesmaPessoa.nome);
+		System.out.println(mesmaPessoa.cpf);
+		System.out.println(mesmaPessoa.idade);
+
+		System.out.println("-------------------");
+		// delete
+		boolean success = manager.deleteObject(mesmaPessoa);
+		System.out.println(success);
 	}
 }
